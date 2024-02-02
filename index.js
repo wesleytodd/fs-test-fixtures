741076f104a9a0c98fa786f30315c513e75d63bf
Wes Todd <wes@wesleytodd.com>
2024-02-02 11:33:21 -0600

    feat: support testName option for creating tmp directories per test

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 'use strict';
 const fs = require('node:fs/promises');
-const path = require('path');
+const path = require('node:path');
+const slugify = require('slugify');
 
-module.exports = (opts = {}) => {
-  const FIX = opts.fixtures || path.join(process.cwd(), 'test', 'fixtures');
-  const TMP = opts.tmp || path.join(process.cwd(), 'test', 'tmp');
+module.exports = function create (opts = {}, setupName) {
+  const cwd = opts.cwd || process.cwd();
+  const FIX = opts.fixtures || path.join(cwd, 'test', 'fixtures');
+  let TMP = opts.tmp;
+  if (!TMP && opts.testName) {
+    TMP = path.join(cwd, 'test', `tmp-${slugify(opts.testName)}`);
+  } else if (!TMP) {
+    TMP = path.join(cwd, 'test', 'tmp');
+  }
 
   // Log can be passed in, default is noop, true uses console.log
   let log;
@@ -18,16 +25,17 @@ module.exports = (opts = {}) => {
   log(`Fixture directory: ${FIX}`);
   log(`Temp directory: ${TMP}`);
 
-  return {
+  const instance = {
     FIX,
     TMP,
     setup: async function setup (name) {
-      await this.teardown();
+      await instance.teardown();
       log(`Setting up fixture: ${name || '<empty>'}`);
       await fs.mkdir(TMP, { recursive: true });
       if (name) {
         await fs.cp(path.join(FIX, name), TMP, { recursive: true });
       }
+      return instance;
     },
     teardown: async function teardown () {
       try {
@@ -39,6 +47,13 @@ module.exports = (opts = {}) => {
           throw e;
         }
       }
+      return instance;
     }
   };
+
+  if (setupName) {
+    return instance.setup(setupName);
+  }
+
+  return instance;
 };
